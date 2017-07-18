a8fd5ba060a3ff2c6c0985f4f1d2d8d15a670dd7
Tim Smith <tsmith@chef.io>
2017-07-18 14:51:58 -0700

    Require Chef 12.1+
    Require a mildly modern version of Chef
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,8 @@ license          'MIT'
 description      'LWRP to manage an f5 BigIP load balancer'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.9'
-issues_url 'https://github.com/swalberg/chef-f5/issues' if respond_to?(:issues_url)
-source_url 'https://github.com/swalberg/chef-f5/' if respond_to?(:source_url)
+issues_url 'https://github.com/swalberg/chef-f5/issues'
+source_url 'https://github.com/swalberg/chef-f5/'
+chef_version '>= 12.1' if respond_to?(:chef_version)
 
 depends 'build-essential'
