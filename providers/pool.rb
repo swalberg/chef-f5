a43b381acae7fe9d826d5bb6b0f8abe726ef8c95
Tim Smith <tsmith@chef.io>
2017-07-18 14:50:48 -0700

    Remove extra state updating and duplicate logging
    Converge_by handles this all
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- providers/pool.rb
+++ providers/pool.rb
@@ -20,24 +20,18 @@ action :create do
   if f5.pool_is_missing?(new_resource.name)
     converge_by("Create pool #{new_resource.name}") do
       f5.create_pool(new_resource.name)
-      new_resource.updated_by_last_action(true)
-      Chef::Log.info("#{new_resource} created pool #{new_resource.name}")
     end
   end
 
   if f5.node_is_missing?(new_resource.host)
     converge_by("Add node #{new_resource.host}") do
       f5.add_node(new_resource.host, new_resource.ip)
-      new_resource.updated_by_last_action(true)
-      Chef::Log.info("#{new_resource} added #{new_resource.host} as a new node")
     end
   end
 
   if f5.pool_is_missing_node?(new_resource.name, new_resource.host)
     converge_by("Add #{new_resource.host} to pool #{new_resource.name}") do
       f5.add_node_to_pool(new_resource.name, new_resource.host, new_resource.port)
-      new_resource.updated_by_last_action(true)
-      Chef::Log.info("#{new_resource} added #{new_resource.host} to pool #{new_resource.name}")
     end
   end
 
@@ -46,8 +40,6 @@ action :create do
       converge_by("Add monitor #{new_resource.monitor} to pool #{new_resource.name}") do
         begin
           f5.add_monitor(new_resource.name, new_resource.monitor)
-          new_resource.updated_by_last_action(true)
-          Chef::Log.info("#{new_resource} added monitor #{new_resource.monitor} to pool #{new_resource.name}")
         rescue StandardError => e
           Chef::Log.info("Adding monitor #{new_resource.monitor} failed. Ensure it exists.")
           Chef::Log.info(e.inspect)
@@ -55,7 +47,7 @@ action :create do
 
       end
     else
-      Chef::Log.info("#{new_resource.name} already has monitor #{new_resource.monitor}")
+      Chef::Log.debug("#{new_resource.name} already has monitor #{new_resource.monitor}")
     end
   end
 end
--- providers/vip.rb
+++ providers/vip.rb
@@ -21,7 +21,6 @@ action :create do
   if f5.vip_is_missing?(new_resource.name)
     converge_by("Create vip #{new_resource.name}") do
       f5.create_vip(new_resource.name, new_resource.pool, new_resource.address, new_resource.port, new_resource.protocol)
-      new_resource.updated_by_last_action(true)
       Chef::Log.info("#{new_resource} created vip #{new_resource.name} at #{new_resource.address}:#{new_resource.port}/#{new_resource.protocol}")
     end
   end
@@ -29,6 +28,4 @@ action :create do
   if f5.vip_default_pool(new_resource.name) != new_resource.pool
     f5.set_vip_pool(new_resource.name, new_resource.pool)
   end
-
 end
-
