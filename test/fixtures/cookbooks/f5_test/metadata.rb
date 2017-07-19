d863fc50a250573258ae5687a0f3e0117adba2fb
Tim Smith <tsmith@chef.io>
2017-07-18 17:21:43 -0700

    Remove readme from the test cookbook metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/fixtures/cookbooks/f5_test/metadata.rb
+++ test/fixtures/cookbooks/f5_test/metadata.rb
@@ -3,7 +3,5 @@ maintainer 'Sean Walberg'
 maintainer_email 'sean@ertw.com'
 license 'MIT'
 description 'For testing the f5 LWRP'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.0'
-
 depends 'f5'
