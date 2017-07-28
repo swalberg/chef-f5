d91384d0224e433f7050a9cac947a93907448b06
Tim Smith <tsmith@chef.io>
2017-07-28 10:32:16 -0700

    Require Chef 12.7+
    This fixes some issues with custom resources
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -2,12 +2,12 @@ name             'f5'
 maintainer       'Sean Walberg'
 maintainer_email 'sean@ertw.com'
 license          'MIT'
-description      'LWRP to manage an F5 BigIP load balancer'
+description      'Resources for managing an F5 BigIP load balancer'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.9'
 issues_url 'https://github.com/swalberg/chef-f5/issues'
 source_url 'https://github.com/swalberg/chef-f5/'
-chef_version '>= 12.6' if respond_to?(:chef_version)
+chef_version '>= 12.7' if respond_to?(:chef_version)
 supports 'centos'
 supports 'amazon'
 supports 'redhat'
