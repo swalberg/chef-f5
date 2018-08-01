1975172b22fa1bd0e669c2302a06b2d0a6c797ba
Chris Usick <christopher.usick@gmail.com>
2018-08-01 10:13:20 -0500

    add IRule resource (#18)

---
+++ test/fixtures/cookbooks/f5_test/recipes/test_create_irule.rb
@@ -0,0 +1,10 @@
+f5_irule 'test-irule' do
+  definition <<-EOL
+# For hosts that serve both http but attached to http and https vips, 
+# this lets them know if the request
+# originally came in on https
+when HTTP_REQUEST {
+  HTTP::header insert HTTPS true
+}
+  EOL
+end
