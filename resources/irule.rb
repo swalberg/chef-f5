598932adb194ef13e34bacc3eff2d3ec1c7fb9c6
Chris Usick <christopher.usick@gmail.com>
2018-08-08 10:25:48 -0500

    allow optional naming differentiation between f5 and chef

--- resources/irule.rb
+++ resources/irule.rb
@@ -1,4 +1,5 @@
 property :name, String
+property :irule_name, [NilClass, String], default: nil
 property :definition, String
 property :load_balancer, String, regex: /.*/, default: 'default'
 property :lb_host, String
@@ -7,25 +8,29 @@ property :lb_password, String
 
 action :create do
   load_f5_gem
+  actual_irule_name = new_resource.irule_name || new_resource.name
   irule = ChefF5::IRule.new(node, new_resource, new_resource.load_balancer)
 
-  if irule.is_missing?(new_resource.name)
-    converge_by "Create IRule #{new_resource.name}" do
-      irule.create(new_resource.name, new_resource.definition)
+  if irule.is_missing?(actual_irule_name)
+    converge_by "Create IRule #{actual_irule_name}" do
+      irule.create(actual_irule_name, new_resource.definition)
       return
     end
   end
 
-  if irule.definition_changed?(new_resource.name, new_resource.definition)
-    converge_by "Update definition of IRule #{new_resource.name}" do
-      irule.update_definition(new_resource.name, new_resource.definition)
+  if irule.definition_changed?(actual_irule_name, new_resource.definition)
+    converge_by "Update definition of IRule #{actual_irule_name}" do
+      irule.update_definition(actual_irule_name, new_resource.definition)
     end
   end
 end
 
 action :destroy do
-  unless irule.is_missing?(new_resource.name)
-    converge_by "Destroy IRule #{new_resource.name}" do
+  actual_irule_name = new_resource.irule_name || new_resource.name
+  irule = ChefF5::IRule.new(node, new_resource, new_resource.load_balancer)
+
+  unless irule.is_missing?(actual_irule_name)
+    converge_by "Destroy IRule #{actual_irule_name}" do
       irule.destroy(name)
     end
   end
--- resources/pool.rb
+++ resources/pool.rb
@@ -1,3 +1,4 @@
+property :pool_name, [NilClass, String], default: nil
 property :host, String, regex: /.*/
 property :ip, String, regex: /.*/
 property :port, [String, Integer], regex: /^(\*|\d+)$/
@@ -10,15 +11,16 @@ property :lb_password, String
 property :enabled_status, [:manual, :enabled, :disabled], default: node['f5']['enabled_status']
 
 create_node = proc do
+  actual_pool_name = new_resource.pool_name || new_resource.name
   if @f5.node_is_missing?(new_resource.host)
     converge_by("Add node #{new_resource.host}") do
       @f5.add_node(new_resource.host, new_resource.ip)
     end
   end
 
-  if @f5.pool_is_missing_node?(new_resource.name, new_resource.host)
-    converge_by("Add #{new_resource.host} to pool #{new_resource.name}") do
-      @f5.add_node_to_pool(new_resource.name, new_resource.host, new_resource.port)
+  if @f5.pool_is_missing_node?(actual_pool_name, new_resource.host)
+    converge_by("Add #{new_resource.host} to pool #{actual_pool_name}") do
+      @f5.add_node_to_pool(actual_pool_name, new_resource.host, new_resource.port)
     end
   end
   if new_resource.enabled_status != :manual
@@ -36,24 +38,25 @@ create_node = proc do
 end
 
 create_pool = proc do
-  if @f5.pool_is_missing?(new_resource.name)
-    converge_by("Create pool #{new_resource.name}") do
-      @f5.create_pool(new_resource.name)
+  actual_pool_name = new_resource.pool_name || new_resource.name
+  if @f5.pool_is_missing?(actual_pool_name)
+    converge_by("Create pool #{actual_pool_name}") do
+      @f5.create_pool(actual_pool_name)
     end
   end
 
   if new_resource.monitor
-    if @f5.pool_is_missing_monitor?(new_resource.name, new_resource.monitor)
-      converge_by("Add monitor #{new_resource.monitor} to pool #{new_resource.name}") do
+    if @f5.pool_is_missing_monitor?(actual_pool_name, new_resource.monitor)
+      converge_by("Add monitor #{new_resource.monitor} to pool #{actual_pool_name}") do
         begin
-          @f5.add_monitor(new_resource.name, new_resource.monitor)
+          @f5.add_monitor(actual_pool_name, new_resource.monitor)
         rescue StandardError => e
           Chef::Log.info("Adding monitor #{new_resource.monitor} failed. Ensure it exists.")
           Chef::Log.info(e.inspect)
         end
       end
     else
-      Chef::Log.debug("#{new_resource.name} already has monitor #{new_resource.monitor}")
+      Chef::Log.debug("#{actual_pool_name} already has monitor #{new_resource.monitor}")
     end
   end
 end
